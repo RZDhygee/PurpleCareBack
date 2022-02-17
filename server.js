31aadc2dc7cff3237752c85e059a4e03aa55713b
mimicha <rzouari@dhygee.ch>
2022-02-17 17:59:22 +0100

    17:59

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//Intiailzie app with express
 const app = express();
-const PORT = 3000;
+const MongoClient = require('mongodb').MongoClient;
 
-app.listen(PORT, (error) =>{
-	if(!error)
-		console.log("Server is Successfully Running and App is listening on port "+ PORT)
-	else
-		console.log("Error occured, server can't start", error);
-	}
-);
+
+//Database connection
+mongoose.Promise = global.Promise; //fix deprecation issue
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Port to be used for the server
+const _PORT= process.env.PORT;
+const mongoURI = process.env.DATABASE;
+
+//Create mongo connection
+const conn = mongoose.createConnection(mongoURI, {useNewUrlParser: true, useUnifiedTopology:true});
+let gfs;
+
+conn.on('connected', () => {
+    console.log('Connected to the database');
+});
+
+conn.on('error', (err) =>{
+    console.log(`Unable to connect to the database: ${err}`);
+});
+
+const client = new MongoClient(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    //perform actions on the collection object
+    client.close();
+});
